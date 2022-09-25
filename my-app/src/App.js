c4f399b1443273749e2d3d670f5bba07af67baf8
Jorge <jorgesilva1892@gmail.com>
2022-09-25 13:18:36 -0700

    app styled working on api call next

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
-
+import Advice from "./components/advice"
+import AdviceBtn from './components/adviceBtn';
+import AdviceNumber from './components/adviceNumber'
+import Divider from "./Assets/pattern-divider-desktop.svg"
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div className="advice-card">
+        <AdviceNumber/>
+        <Advice/>
+        <img src={Divider} alt="divider"/>
+      </div>
+      <AdviceBtn/>
     </div>
   );
 }
---
+++ my-app/src/components/adviceNumber.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const adviceNumber = () => {
+  return <div className="adviceNumber"> ADVICE #117</div>;
+}
+
+export default adviceNumber
